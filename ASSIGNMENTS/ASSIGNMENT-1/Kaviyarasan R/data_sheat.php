1dbaacaf35165d2a2f25eac56f34815fc070de87
arunkumarskcse <114871881+arunkumarskcse@users.noreply.github.com>
2022-10-18 11:47:56 -0700

    Add files via upload

---
+++ ASSIGNMENTS/ASSIGNMENT-1/Kaviyarasan R/data_sheat.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form action="hello.php" method="post">
+  Name: <input type="text" name="fname">
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
---
+++ ASSIGNMENTS/ASSIGNMENT-2/T.Nitheshwaran/Flask/put - Copy.py
@@ -0,0 +1,11 @@
+@app.route("/guide/<id>", methods=["PUT"])
+def guide_update(id):
+    guide = Guide.query.get(id)
+    title = request.json['title']
+    content = request.json['content']
+
+    guide.title = title
+    guide.content = content
+
+    db.session.commit()
+    return guide_schema.jsonify(guide)
